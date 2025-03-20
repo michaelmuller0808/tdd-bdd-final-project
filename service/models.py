9eed847d72bd9621e7593f911e51f34052665ebe
PAKOU Komi Juste <118111865+Juste120@users.noreply.github.com>
2025-03-20 07:24:57 -0400

    the final project

--- service/models.py
+++ service/models.py
@@ -247,3 +247,6 @@ class Product(db.Model):
         """
         logger.info("Processing category query for %s ...", category.name)
         return cls.query.filter(cls.category == category)
+
+
+ 
